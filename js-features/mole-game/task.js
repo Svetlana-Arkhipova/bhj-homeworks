f1e02fa9292166e09026c1b39c7666f4cfd7a815
Svetlana Arkhipova <sve.tochka@inbox.ru>
2020-06-30 22:22:03 +0300

    Доработка задачи 3, onclick

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,30 +2,48 @@ const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 let deadCounter = +dead.textContent;
 let lostCounter = +lost.textContent;
-//let field = [];
-
-// for (let i = 0; i <= 8; i++) {
-//   field[i] = getHole(i + 1);
-// }
-for (let i = 1; i <= 9; i++) {
-  hole = getHole(i);
-  if (hole.className.includes('hole_has-mole')) {
-    hole.onclick = function() {
-      deadCounter += 1;
-      dead.textContent = deadCounter;
-    }
-  } else {
-    hole.onclick = function() {
-      lostCounter += 1;
-      lost.textContent = lostCounter;
-    }
-  }
-  if (deadCounter === 10) {
-    alert('Вы выиграли!');
-  }
-}
-
 
 function getHole(index) {
   return document.getElementById(`hole${index}`);
 }
+
+  //hole = getHole(1);
+
+  hole.onclick = function() {
+    if (hole.className.includes('hole_has-mole')) {
+      deadCounter += 1;
+      dead.textContent = deadCounter;
+    } else {
+      lostCounter += 1;
+      lost.textContent = lostCounter;
+    }
+    if (lostCounter === 5 || deadCounter === 10) {
+      if (lostCounter === 5) {alert('Вы проиграли(((');}
+      if (deadCounter === 10) {alert('Вы выиграли!');}
+      lostCounter = 0;
+      lost.textContent = lostCounter;
+      deadCounter = 0;
+      dead.textContent = deadCounter;
+    }
+  }
+
+
+
+
+// for (let i = 1; i <= 9; i++) {
+//   hole = getHole(i);
+//   if (hole.className.includes('hole_has-mole')) {
+//     hole.onclick = function() {
+//       deadCounter += 1;
+//       dead.textContent = deadCounter;
+//     }
+//   } else {
+//     hole.onclick = function() {
+//       lostCounter += 1;
+//       lost.textContent = lostCounter;
+//     }
+//   }
+//   if (deadCounter === 10) {
+//     alert('Вы выиграли!');
+//   }
+// }
