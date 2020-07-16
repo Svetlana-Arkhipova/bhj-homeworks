21386d0141df2367eecea905b7179bd60b9be52f
Svetlana Arkhipova <sve.tochka@inbox.ru>
2020-07-16 20:23:06 +0300

    Доработка задач "Меню" и "Слайдер"

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,16 @@
-let links = document.querySelectorAll('.menu__link');
+let links = document.querySelectorAll('ul.menu.menu_main > li.menu__item > a.menu__link');
 let arrLinks = Array.from(links);
-//console.log(arrLinks);
 
 arrLinks.forEach((item) => {
-  if(item.closest('.menu.menu_sub')) {
-    let a = item.closest('.menu__item');
-    let b = a.closest('.menu__link');
-    //console.log(b);
-    b.onclick = function() {
-      item.closest('.menu.menu_sub').classList.add('.menu_active');
+  let subMenu = item.nextElementSibling;
+  if (subMenu != null) {
+    item.onclick = function() {
+      let activeSubMenu = document.querySelector('.menu_active');
+      if (activeSubMenu != null) {
+          activeSubMenu.classList.remove('menu_active');
+      }
+      subMenu.classList.add('menu_active');
+      return false;
     }
   }
 });
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,50 +1,44 @@
 let prev = document.querySelector('.slider__arrow.slider__arrow_prev');
 let next = document.querySelector('.slider__arrow.slider__arrow_next');
-let slides = document.getElementsByClassName('slider__item')
+let slides = document.getElementsByClassName('slider__item');
+let slidesArr = Array.from(slides);
 let sliderDots = document.getElementsByClassName('slider__dot');
 sliderDots[0].classList.add('slider__dot_active');
 
-next.onclick = function() {
-  for (let i = 0; i < slides.length; i++) {
-    if (slides[i].classList.contains('slider__item_active') && i < slides.length - 1) {
-      slides[i].classList.remove('slider__item_active');
-      slides[i+1].classList.add('slider__item_active');
-      sliderDots[i].classList.remove('slider__dot_active');
-      sliderDots[i+1].classList.add('slider__dot_active');
-      break;
-    } else if (i === slides.length - 1) {
-      slides[i].classList.remove('slider__item_active');
-      slides[0].classList.add('slider__item_active');
-      sliderDots[i].classList.remove('slider__dot_active');
-      sliderDots[0].classList.add('slider__dot_active');
-    }
-  }
+function setActive(number) {
+  slides[number].classList.add('slider__item_active');
+  sliderDots[number].classList.add('slider__dot_active');
 }
 
-prev.onclick = function() {
-  for (let i = slides.length - 1; i >= 0; i--) {
-    if (slides[i].classList.contains('slider__item_active') && i > 0) {
-      slides[i].classList.remove('slider__item_active');
-      slides[i-1].classList.add('slider__item_active');
-      sliderDots[i].classList.remove('slider__dot_active');
-      sliderDots[i-1].classList.add('slider__dot_active');
-      break;
-    } else if (i === 0) {
-      slides[i].classList.remove('slider__item_active');
-      slides[slides.length-1].classList.add('slider__item_active');
-      sliderDots[i].classList.remove('slider__dot_active');
-      sliderDots[slides.length-1].classList.add('slider__dot_active');
+function removeActive(number) {
+  slides[number].classList.remove('slider__item_active');
+  sliderDots[number].classList.remove('slider__dot_active');
+}
+
+next.onclick = function() {
+  let index = slidesArr.findIndex((item) => item.classList.contains('slider__item_active'));
+  removeActive(index);
+  if (index < slides.length - 1) {
+    setActive(index + 1);
+  } else {
+    setActive(0);
     }
   }
-}
+
+prev.onclick = function() {
+  let index = slidesArr.findIndex((item) => item.classList.contains('slider__item_active'));
+  removeActive(index);
+  if (index > 0) {
+    setActive(index - 1);
+  } else {
+    setActive(slides.length - 1);
+    }
+  }
 
 for (let i = 0; i < sliderDots.length; i++) {
   sliderDots[i].onclick = function() {
-    for (let j = 0; j < sliderDots.length; j++) {
-      slides[j].classList.remove('slider__item_active');
-      sliderDots[j].classList.remove('slider__dot_active');
-    }
-    slides[i].classList.add('slider__item_active');
-    sliderDots[i].classList.add('slider__dot_active');
+    let index = slidesArr.findIndex((item) => item.classList.contains('slider__item_active'));
+    removeActive(index);
+    setActive(i);
   }
 }
